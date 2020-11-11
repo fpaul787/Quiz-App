aa93b9562c5d20ab64a29cb5f13e8fd3311f6061
Frantz Paul <fpaul787@gmail.com>
2020-11-10 22:49:21 -0500

    liking typescrpt so far, skeleton created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+// Components
+import QuestionCard from './components/QuestionCard';
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e : React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
 
 
-function App() {
+  }
   return (
     <div className="App">
-      Quiz
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Question ...</p>
+      <QuestionCard 
+        questionNr={number + 1}
+        totalQuesetions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
