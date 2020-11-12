0b40f9f39a4fc1afbc7ef49cde7f082776b18aaf
Frantz Paul <fpaul787@gmail.com>
2020-11-11 22:55:27 -0500

    added key for mapping

--- src/App.tsx
+++ src/App.tsx
@@ -46,6 +46,24 @@ const App = () => {
 
   const checkAnswer = (e : React.MouseEvent<HTMLButtonElement>) => {
 
+      if(!gameOver){
+        // users answer
+        const answer = e.currentTarget.value
+        // Check answer against correct answer
+        const correct = questions[number].correct_answer === answer;
+        // Add score if answer is correct
+        if(correct) {
+          setScore(prev => prev + 1);
+        }
+        // Save answer in the array for user answers
+        const answerObject = {
+          question: questions[number].question,
+          answer,
+          correct,
+          correctAnswer: questions[number].correct_answer,
+        };
+        setUserAnswers((prev) => [...prev, answerObject])
+      }
   }
 
   const nextQuestion = () => {
